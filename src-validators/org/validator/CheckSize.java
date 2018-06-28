39ba5c94445fc3842b5c7cd33c7bcb5bbd1f9819
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-06-28 15:35:36 +0200

    v6 forms

---
+++ src-validators/org/validator/CheckSize.java
@@ -0,0 +1,22 @@
+package org.validator;
+
+import java.util.Locale;
+
+import org.bonitasoft.forms.client.model.FormFieldValue;
+import org.bonitasoft.forms.server.validator.IFormFieldValidator;
+
+public class CheckSize implements IFormFieldValidator {
+
+	@Override
+	public String getDisplayName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean validate(FormFieldValue fieldInput, Locale locale) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
